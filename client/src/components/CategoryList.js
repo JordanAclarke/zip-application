d8bff62f106db3495baf248e6fa2fbdd5822a30a
JordanAclarke <JordanAclarke@gmail.com>
2019-08-13 10:08:58 -0400

    category list

---
+++ client/src/components/CategoryList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+export default class CategoryList extends Component {
+    state = {
+        categories: []
+    }
+    componentDidMount() {
+        this.fetchAllCategories
+    }
+    fetchAllCategories = () => {
+        axios.get('/api/v1/categories/')
+            .then((res) => {
+                this.setState({categories: res.data})
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+    render() {
+        let categoryList = this.state.categories.map((category) => {
+            return (
+                <div>
+                    <Link to ={`/categories/${category.id}/`}>
+                        <h2>{category.cate_title}</h2>
+                        <img src={category.photo_url} alt={category.description} />
+                    </Link>
+                </div>
+            )
+        })
+        return (
+            <div>
+                {categoryList}
+            </div>
+        )
+    }
+}
