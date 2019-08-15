71d3f075dc60b0318e1068abc39b9f655e4db698
JordanAclarke <JordanAclarke@gmail.com>
2019-08-15 13:55:17 -0400

    category styling

--- client/src/components/CategoryList.js
+++ client/src/components/CategoryList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
-import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import {Navbar, Nav, NavDropdown, Card, Button} from 'react-bootstrap';
 export default class CategoryList extends Component {
     state = {
         categories: []
@@ -22,10 +22,21 @@ export default class CategoryList extends Component {
         let categoryList = this.state.categories.map((category) => {
             return (
                 <div>
-                    <Link to ={`/categories/${category.id}/`}>
-                        <h2>{category.cate_title}</h2>
-                        <img src={category.photo_url} alt={category.description} />
+                <Link to ={`/categories/${category.id}/`}>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={category.photo_url} />
+                        <Card.Body>
+                            <Card.Title>{category.cate_title}</Card.Title>
+                            <Card.Text>
+                            {category.description}
+                            </Card.Text>
+                            <Button variant="primary">View {category.cate_title} Posts</Button>
+                        </Card.Body>
+                        </Card>
                     </Link>
+                        {/* <h2>{category.cate_title}</h2>
+                        <img src={category.photo_url} alt={category.description} /> */}
+                    
                 </div>
             )
         })
@@ -33,15 +44,18 @@ export default class CategoryList extends Component {
             <div>
                 <Navbar bg="light" expand="lg">
                 <NavDropdown title="≡" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/">Home</NavDropdown.Item>
                 <NavDropdown.Item href="/categories">Categories</NavDropdown.Item>
                 </NavDropdown>
-                <Navbar.Brand href="#home">ZIP</Navbar.Brand>
+                <Navbar.Brand href="#home">ZIP <i class="fa fa-bolt"></i></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                     </Nav>
                 </Navbar.Collapse>
                 </Navbar>
+
+                <h1>Topics</h1>
                 <Link to={'/categories/new/'}>Create New Category</Link>
                 {categoryList}
             </div>
